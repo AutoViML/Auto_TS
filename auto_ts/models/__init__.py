5af17287e6db0c91c87926f5aadb68329a4d5629
Nikhil Gupta <mywork.ng@gmail.com>
2020-06-14 13:49:50 -0500

    added SARIMAX class

--- auto_ts/models/__init__.py
+++ auto_ts/models/__init__.py
@@ -1,4 +1,5 @@
-from .ar_based import build_arima_model, build_sarimax_model, build_var_model
+# from .ar_based import build_arima_model, build_sarimax_model, build_var_model
+from .ar_based import build_arima_model, BuildSarimax, build_var_model
 from .build_ml import run_ensemble_model
 # from .build_prophet import build_prophet_model
 from .build_prophet import BuildProphet
