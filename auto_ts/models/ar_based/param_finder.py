1911d351f90ad2e2958bf9579afb320189295647
Nikhil Gupta <mywork.ng@gmail.com>
2020-06-24 06:28:26 -0500

    TODO comment cleanup

--- auto_ts/models/ar_based/param_finder.py
+++ auto_ts/models/ar_based/param_finder.py
@@ -76,7 +76,7 @@ def find_best_pdq_or_PDQ(ts_train, metric, p_max, d_max, q_max, non_seasonal_pdq
                                     simple_differencing=False, trend='ct',
                                     start_params=[0, 0, 0, 1]
                                         )
-                    results = model.fit()  # TODO: Result is not used, hence commenting 
+                    results = model.fit()   
 
                     results_bic.loc['AR{}'.format(p_val), 'MA{}'.format(q_val)] = eval('results.' + metric)
                     if iteration % 10 == 0:
