a558528767fd57285ae5fc04bcbc1820de527665
AutoViML <rsesha2001@yahoo.com>
2023-02-22 08:30:06 -0500

    new version of prophet 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="auto_ts",
-    version="0.0.68",
+    version="0.0.69",
     author="Ram Seshadri",
     # author_email="author@example.com",
     description="Automatically Build Multiple Time Series models fast - now with Facebook Prophet!",
